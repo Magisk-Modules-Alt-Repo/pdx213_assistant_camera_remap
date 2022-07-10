974ada215da89f65b8bb160af22422556695824e
Jakob Fridesjö <tfy17jfo@cs.umu.se>
2022-07-11 01:19:35 +0200

    Changed device identifying method

--- customize.sh
+++ customize.sh
@@ -1,6 +1,6 @@
-DEVICE="Pdx213"
+DEVICE="pdx213"
 
-if [ "$(getprop ro.build.product)" != "$DEVICE" ]; then
-    ui_print "This module is meant for Xperia 10 III (Pdx213)"
+if [ "$(getprop ro.product.device)" != "$DEVICE" ]; then
+    ui_print "This module is meant for Xperia 10 III (pdx213)"
     abort "Aborting Module Installation"
 fi;
