afd5a1cff3ed7bcded3b05e8d1b9f1bdec58505c
Jakob Fridesjö <tfy17jfo@cs.umu.se>
2022-07-17 23:55:45 +0200

    minor fix

--- customize.sh
+++ customize.sh
@@ -1,4 +1,4 @@
-if [ "$(getprop ro.product.device)" != "pdx213" ] || [ "$(getprop ro.product.device)" != "XQ-BT52" ]; then
+if [ "$(getprop ro.product.device)" != "pdx213" ] && [ "$(getprop ro.product.device)" != "XQ-BT52" ]; then
     ui_print "This module is meant for Xperia 10 III (pdx213/XQ-BT52)"
     abort "Aborting Module Installation"
 fi;
