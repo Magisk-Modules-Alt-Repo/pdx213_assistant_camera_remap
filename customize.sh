f725ab35649883445eb7e1464266f741b012d7aa
Jakob Fridesjö <tfy17jfo@cs.umu.se>
2022-07-17 23:47:00 +0200

    added alternative device id

--- customize.sh
+++ customize.sh
@@ -1,6 +1,4 @@
-DEVICE="pdx213"
-
-if [ "$(getprop ro.product.device)" != "$DEVICE" ]; then
-    ui_print "This module is meant for Xperia 10 III (pdx213)"
+if [ "$(getprop ro.product.device)" != "pdx213" ] || [ "$(getprop ro.product.device)" != "XQ-BT52" ]; then
+    ui_print "This module is meant for Xperia 10 III (pdx213/XQ-BT52)"
     abort "Aborting Module Installation"
 fi;
